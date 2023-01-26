d56700836a093173259aa518d3b750683c29c0c6
SainasFede <sainasfede@gmail.com>
2023-01-26 16:43:48 +0100

    Milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script>
-export default {
 
+import axios from 'axios';
+
+export default {
+  name:'App',
+  data(){
+    return {
+      baseUrl:'http://127.0.0.1:8000/api',
+      pUrl :'/project',
+      projects : [],
+    }
+  },
+  methods:{
+     getApi(){
+        axios.get(this.baseUrl + this.pUrl)
+        .then(result=>{
+        this.projects = result.data
+        console.log(this.projects);
+       })
+     }
+  },
+  mounted(){
+    this.getApi()
+  }
 }
 </script>
 
 <template>
   <div class="container">
-    <h1>Ciao</h1>
+    <h1>ciao</h1>
   </div>
 </template>
 
