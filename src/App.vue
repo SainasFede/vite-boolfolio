75c21a90c585b54ec121ef4f9fcbe30767d063ab
SainasFede <sainasfede@gmail.com>
2023-01-26 15:37:25 +0100

    Milestone 3

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1>Ciao</h1>
+  </div>
+</template>
+
+<style lang="scss">
+    @use "./style/general.scss"
+</style>
