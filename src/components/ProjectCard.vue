d227a50175c4cf42bcbe385df52c37099df645d4
SainasFede <sainasfede@gmail.com>
2023-01-27 15:03:58 +0100

    add Router

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,11 +13,12 @@ export default {
 </script>
 
 <template>
-  <div class="card" style="width: 18rem;">
+  <div class="card m-2" style="width: 18rem;">
   <img src="{{project.cover_image}}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">{{project.name}}</h5>
     <h5 class="card-title">{{project.client_name}}</h5>
+    <h6 class="card-title">{{project.technologies.type}}</h6>
     <h6 class="card-title">{{project.category.type}}</h6>
     <p>{{project.summary}}</p>
   </div>
---
+++ src/pages/Contacts.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Contacts'
+}
+</script>
+
+<template>
+  <h1>Contatti</h1>
+</template>
+
+<style>
+
+</style>
